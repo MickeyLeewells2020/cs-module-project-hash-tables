217181021268ee2219574b2ab317186c34417b02
MickeyLeeWells2020 <mlwells1051@gmail.com>
2020-09-08 13:31:56 -0600

    got them all fixed and pushed after  crash

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,4 +1,38 @@
 def word_count(s):
+    s = s.lower()
+    ignore = [
+        '"',
+        ":",
+        ";",
+        ",",
+        ".",
+        "-",
+        "+",
+        "=",
+        "/",
+        "\\",
+        "|",
+        "[",
+        "]",
+        "{",
+        "}",
+        "(",
+        ")",
+        "*",
+        "^",
+        "&",
+    ]
+
+    for char in ignore:
+        s = s.replace(char, "")
+    counts = {}
+    split = s.split()
+    for word in split:
+        if word in counts.keys():
+            counts[word] += 1
+        else:
+            counts[word] = 1
+    return counts
     # Your code here
 
 
